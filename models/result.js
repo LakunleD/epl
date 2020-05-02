f5d8fc3aea2d4e52666c27971b174338aa9f7e9f
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2020-05-02 16:12:16 +0100

    result model

---
+++ models/result.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const joi = require('@hapi/joi');
+const joigoose = require("joigoose")(mongoose, null, {
+    _id: false,
+    timestamps: false
+});
+
+const JoiResultSchema = joi.object({
+    home_goal: joi.number().required(),
+    away_goal: joi.number().required(),
+    home_id: joi.string().meta({ _mongoose: { type: "ObjectId", ref: "Team" } }),
+    away_id: joi.string().meta({ _mongoose: { type: "ObjectId", ref: "Team" } })
+});
+
+const MongooseResultSchema = new mongoose.Schema(joigoose.convert(JoiResultSchema));
+
+const Result = mongoose.model('Result', MongooseResultSchema);
+
+exports.Result = Result;
